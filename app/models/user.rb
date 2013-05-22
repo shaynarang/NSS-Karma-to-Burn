fa55795c703b7e145e200b74a3abcdb2a5c5de4a
shaynarang <shaynarang@gmail.com>
2013-05-22 12:32:46 -0500

    fixed return user issue in greeting method of ktb.rb and remove method in daily_check_in_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessor :current_user
-  has_many :daily_check_ins
+  has_many :daily_check_ins, :dependent => :destroy
   validates_uniqueness_of :name, message: "must be unique"
 end
