0bf96b494983bea4a366880224420d44a58c329e
shaynarang <shaynarang@gmail.com>
2013-05-25 16:57:48 -0500

    added, separated, and restructured methods in ktb.rb

---
+++ test/unit/test_generate_greeting.rb
@@ -0,0 +1,19 @@
+require_relative '../test_helper'
+
+# class TestGenerateGreeting < Test::Unit::TestCase
+#   include DatabaseCleaner
+
+#   def test_existing_user_returns_existing_welcome_back_message
+#     check_in = DailyCheckIn.new
+#     User.create(name: "Bob", date_of_birth: 1934-12-12)
+#     greeting = check_in.generate_greeting("Bob", "1934-12-12")
+#     assert_equal("Welcome back, Bob.\n", greeting)
+#   end
+
+  # def test_new_user_is_created
+  #   check_in = DailyCheckIn.new
+  #   greeting = check_in.generate_greeting("Bob", "1934-12-12")
+  #   assert_equal("Greetings, Bob.\n", greeting)
+  # end
+
+end
